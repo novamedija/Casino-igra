ccb0a6930768a27648a7e9aa66147a1140d0a5d5
novamedija <kuldija_alen@hotmail.com>
2015-09-07 00:42:16 +0200

    Create casino.py

---
+++ casino.py
@@ -0,0 +1,13 @@
+# -*- coding: <utf-8> -*-
+__author__ = 'Alen'
+
+skritastevilka = 6421
+
+while True:
+    skritastevilka = raw_input("Ugani številko: ")
+
+    if skritastevilka == "6421":
+        print ("Čestitke! Uganili ste skrito število!")
+
+    else:
+        print ("Žal ste vnesli napačno število.")
